2b23f9bfce2003ef6095eb472b4442e068f4b813
wh3pp <wh3pp@proton.me>
2025-07-30 10:15:26 -0500

    Actualizar configuración, dependencias y estructura base

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -1,9 +1,11 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
+// See https://svelte.dev/docs/kit/types#app-d-ts
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			usuario: import('./lib/types').Usuario | null;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
